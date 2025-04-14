1231d3fbf288f8008b428858105da3967697b3a2
Karyna Shevchenko <shevchenkoks1810@gmail.com>
2025-04-14 13:07:57 +0300

    Stage 5/5

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,8 +1,13 @@
 package cinema;
+
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Cinema {
     static Scanner scanner = new Scanner(System.in);
+    static int purchasedTickets = 0;
+    static int currentIncome = 0;
 
     public static void main(String[] args) {
 
@@ -24,12 +29,37 @@ public class Cinema {
                 case 2:
                     buyTicket(cinema, rows, seats);
                     break;
+                case 3:
+                    showStatistics(rows, seats);
+                    break;
                 case 0:
                     return;
                 default:
                     System.out.println("Invalid option selected. Try again.");
             }
         }
+
+    }
+
+    private static void showStatistics(int rows,int seats) {
+        int totalSeats = rows * seats;
+        int totalIncome;
+
+        if (totalSeats <= 60){
+            totalIncome = totalSeats * 10;
+
+        } else {
+            int frontRows = rows / 2;
+            int backRows = rows - frontRows;
+
+            totalIncome = frontRows * seats * 10 + backRows * seats * 8 ;
+        }
+        double percentagePurchTickets = purchasedTickets * 100.00 / totalSeats;
+
+        System.out.println("\nNumber of purchased tickets: " + purchasedTickets);
+        System.out.printf(Locale.US, "Percentage: %.2f%%\n", percentagePurchTickets);
+        System.out.println("Current income: $" + currentIncome);
+        System.out.println("Total income: $" + totalIncome);
 
     }
 
@@ -46,23 +76,37 @@ public class Cinema {
     public static void printMenu() {
         System.out.println("\n1. Show the seats");
         System.out.println("2. Buy a ticket");
+        System.out.println("3. Statistics");
         System.out.println("0. Exit");
 
     }
+
     public static void buyTicket(String[][] cinema, int rows, int seats){
+        while (true) {
             System.out.println("\nEnter a row number:");
             int rowSelected = scanner.nextInt();
             System.out.println("Enter a seat number in that row:");
             int seatSelected = scanner.nextInt();
 
-            //ticket price
-            int price = calculateTicketPrice(rows, seats, rowSelected);
-            System.out.println("\nTicket price: $" + price);
+            if (rowSelected < 1 || rowSelected > rows || seatSelected < 1 || seatSelected > seats) {
+                System.out.println("\nWrong input!");
+                continue;
+            }
 
+            if (Objects.equals(cinema[rowSelected - 1][seatSelected - 1], "S")) {
 
-            //print cinema scheme
-            cinema[rowSelected - 1][seatSelected - 1] = "B";
+                int price = calculateTicketPrice(rows, seats, rowSelected);
+                System.out.println("\nTicket price: $" + price);
+
+                cinema[rowSelected - 1][seatSelected - 1] = "B";
+                purchasedTickets += 1;
+                currentIncome += price;
+                break;
+            } else {
+                System.out.println("\nThat ticket has already been purchased!");
+            }
         }
+    }
 
     public static int calculateTicketPrice(int rows, int seats, int rowSelected) {
             int totalSeats = rows * seats;
@@ -87,24 +131,6 @@ public class Cinema {
                 }
                 System.out.println();
             }
-        /*
-        int totalSeats = rows * seats;
-        int income;
-
-        if (totalSeats <= 60){
-            income = totalSeats * 10;
-
-        } else {
-            int frontRows = rows / 2;
-            int backRows = rows - frontRows;
-
-            income = frontRows * seatsInRow * 10 + backRows * seatsInRow * 8 ;
-        }
-
-        System.out.println("Total income:");
-        System.out.println("$" + income);
-
-         */
 
     }
 }
